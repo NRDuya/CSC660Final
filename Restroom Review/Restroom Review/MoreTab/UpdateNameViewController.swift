90da9afd2bc239bc9db8add585015f60d5a02bdd
Nathaniel Duya <nathanielduya@yahoo.com>
2022-04-21 23:42:44 -0700

    Added change display name

--- Restroom Review/Restroom Review/MoreTab/UpdateNameViewController.swift
+++ Restroom Review/Restroom Review/MoreTab/UpdateNameViewController.swift
@@ -11,11 +11,33 @@ import FirebaseAuthUI
 class UpdateNameViewController: UIViewController {
     let userModel = UserModel()
     
+    @IBOutlet weak var displayName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func updateName(_ sender: UIButton) {
+        if let name = displayName.text, name.isEmpty {
+            dismiss(animated: true)
+            return
+        }
 
-
+        let user = Auth.auth().currentUser
+        guard let user = user, let name = displayName.text, name != user.displayName else {
+            return
+        }
+        let profileChangeRequest = user.createProfileChangeRequest()
+        profileChangeRequest.displayName = name
+        Task {
+            do {
+                try await profileChangeRequest.commitChanges()
+                
+            } catch {
+                print(error)
+            }
+        }
+        userModel.logUser(uid: user.uid, displayName: name)
+        dismiss(animated: true)
+    }
 }
